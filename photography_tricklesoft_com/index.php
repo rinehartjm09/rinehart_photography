c17958ac29094b40c4ce2e1bf1d5e34a7e2b7e53
Joe Rinehart <rinehartjm09@gmail.com>
2019-01-20 13:46:19 -0800

    header image

--- photography_tricklesoft_com/index.php
+++ photography_tricklesoft_com/index.php
@@ -1,4 +1,15 @@
 <?php
 include 'includes/header.php'
 ?>
-<h1>Hello welcome to my page</h1>
+<figure id="header-img-parent">
+<img id="header-img" src="./images/bridgeval2.jpg" alt="Lewiston/Clarkston Blue Bridge">
+  <figcaption>Rinehart Photography</figcaption>
+</figure>
+<div class="container">
+
+  <div class="row">
+    <div class="col">
+      <h1>Rinehart Photography</h1>
+    </div>
+  </div>
+</div>
